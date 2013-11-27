0550573f0a844ae8c1a06802dff37dd32b88b3de
Diogo Pereira <diogo.pereira@hp.com>
2013-11-27 14:24:46 -0700

    updating it for next vacations, and making it easier to change :)

--- waning/waning.js
+++ waning/waning.js
@@ -1,12 +1,30 @@
-var increment = 1/766611.0;
-var expiration = 1369931400;
+var makeLinearFunction = function (x1, y1, x2, y2) {
+	var deltaY = y2 - y1;
+	var deltaX = x2 - x1;
+	var slope = deltaY/deltaX;
+	var adjustment = y1 - (x1 * slope);
+	return function (x) {
+		return slope * x + adjustment;
+	};
+};
 
-var wane = function () {
-	var current = Math.floor((new Date()).getTime() / 1000);
-	var opacity = Math.max((expiration - current)*increment, 0);
-	
-	$('#text').css({ 'opacity': opacity });
+var makeLinearFadeFunction = function (start, end) {
+	return makeLinearFunction(start, 100, end, 0);
+};
+
+var toSeconds = function (milliseconds) {
+	return Math.floor(milliseconds / 1000);
+};
+
+var start = toSeconds(new Date(2013, 10, 27).getTime()); // why the hell months have 0-based indexes??
+var end = toSeconds(new Date(2013, 11, 21).getTime());
+var wane = makeLinearFadeFunction(start, end);
+
+var waneDOM = function () {
+	var current = toSeconds(new Date().getTime());
+	var opacity = wane(current);
+	$("#text").css({ "opacity": opacity });
 }
 
-var waning = setInterval(wane, 1000);
-wane();
+var waning = setInterval(waneDOM, 1000);
+waneDOM();
