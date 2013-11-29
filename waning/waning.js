cc4a17c4a83ef6fa8341e6306a734c887870a617
Diogo Pereira <diogo.pereira@hp.com>
2013-11-29 12:53:27 -0700

    adding progress indicator, changing footnotes every 5 seconds

--- waning/waning.js
+++ waning/waning.js
@@ -21,10 +21,44 @@ var end = toSeconds(new Date(2013, 11, 21).getTime());
 var wane = makeLinearFadeFunction(start, end);
 
 var waneDOM = function () {
+	var enforceLimits = function (value, min, max) {
+		var atLeastMin = Math.max(value, min);
+		var atMostMax = Math.min(atLeastMin, max);
+		return atMostMax;
+	};
 	var current = toSeconds(new Date().getTime());
-	var opacity = wane(current);
+	var opacity = enforceLimits(wane(current), 0, 100);
+	var progress = (100 - opacity).toFixed(5);
 	$("#text").css({ "opacity": opacity });
-}
+	$("#progress").text(progress + "%");
+};
+
+var changeFootnote = (function () {
+	var counter = 1;
+	var change = function () {
+		var text = "";
+		switch (counter) {
+			case 0:
+			    text = "...it takes a while, though";
+			    break;
+			case 1:
+			    text = "...but it will keep going on";
+			    break;
+			case 2:
+			    text = "...even if you close this website";
+			    break;
+			case 3:
+			    text = "...it's bound to keep waning";
+			    break;
+		}
+		$("#footnote p:first-child").text(text);
+		counter += 1;
+		counter = counter % 4;
+	};
+
+	return change;
+}());
 
 var waning = setInterval(waneDOM, 1000);
+var footnote = setInterval(changeFootnote, 5000);
 waneDOM();
